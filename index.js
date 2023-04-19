7baf40b2d700f918b50450fd454c38625669b94e
unknown <ravikumargupta391999@gmail.com>
2023-04-19 21:39:24 +0530

    Controller and Router

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const port = 8000;
 // Use express router
 app.use('/',require('./routes')); 
 
+// set up the engine 
+app.set('view engine','ejs');
+app.set('views','./views');
+
+
+
 app.listen(port, function(err){
 
     if(err){
