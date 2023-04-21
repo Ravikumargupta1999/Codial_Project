c4fc98470d8a071d7ffd6c515e1d7e3e996cd8c7
unknown <ravikumargupta391999@gmail.com>
2023-04-21 23:36:35 +0530

    Rendering Pages for Sign up and Sign In

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,17 @@ module.exports.profile = function(req,res){
         title : "User Profile"
     });
 }
+
+// rendering sign up page
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up',{
+        title : "Codial | Sign UP"
+    })
+};
+
+// rendering sign in page
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in',{
+        title : "Codial | Sign In"
+    })
+};
