14e8a0e63f703bcef42dd92354e59ff966922454
unknown <ravikumargupta391999@gmail.com>
2023-04-22 12:53:47 +0530

    Sign Up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,32 +1,55 @@
-module.exports.profile = function(req,res){
-    return res.render('user_profile',{
-        title : "User Profile"
+const User = require('../model/user')
+
+module.exports.profile = function (req, res) {
+    return res.render('user_profile', {
+        title: "User Profile"
     });
 }
 
 // rendering sign up page
-module.exports.signUp = function(req,res){
-    return res.render('user_sign_up',{
-        title : "Codial | Sign UP"
+module.exports.signUp = function (req, res) {
+    return res.render('user_sign_up', {
+        title: "Codial | Sign UP"
     })
 };
 
 // rendering sign in page
-module.exports.signIn = function(req,res){
-    return res.render('user_sign_in',{
-        title : "Codial | Sign In"
+module.exports.signIn = function (req, res) {
+    return res.render('user_sign_in', {
+        title: "Codial | Sign In"
     })
 };
 
 
 // get the sign up data for user
-module.exports.create = function(req,res){
-    // TODO later
+module.exports.create = async function (req, res) {
+    if (req.body.password != req.body.confirm_password) {
+        return res.redirect('back');
+    }
+
+    try{
+        let user = await User.findOne({ email: req.body.email });
+
+        if(user)
+            return await res.redirect('back');
+
+        try{
+            User.create(req.body);
+
+            return res.redirect('/users/sign-in');
+        }
+        catch(err){
+            console.log('error in creating user while signing up');
+        }
+    }
+    catch(err){
+        console.log('error in finding user in signing up');
+    }   
 }
 
 
 
 // get the sign in data for user
-module.exports.createSession = function(req,res){
+module.exports.createSession = function (req, res) {
     // TODO later
 }
