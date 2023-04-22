00154001b2cb14c1119752a5a0ebbc288d4b0bd2
unknown <ravikumargupta391999@gmail.com>
2023-04-22 19:23:18 +0530

    Setting Current Authenticated user

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const possport = require('passport');
 const usersController = require('../controllers/users_controller');
 const passport = require('passport');
 
-router.get('/profile',usersController.profile);
+router.get('/profile',passport.checkAuthentication,usersController.profile);
 
 router.get('/sign-up',usersController.signUp);
 router.get('/sign-in',usersController.signIn);
