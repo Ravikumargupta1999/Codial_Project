6c6878f15655de3c96acd36f4ad924b57c911a2f
unknown <ravikumargupta391999@gmail.com>
2023-04-30 13:37:13 +0530

    More Converting to async await

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -26,24 +26,24 @@ module.exports.create = async function(req,res){
       
 }
 
-module.exports.destroy = function(req, res){
-  Post.findById(req.params.id)
-  .then(function(post){
-      // .id means converting the object id into string
-      if (post.user == req.user.id){
-          post.deleteOne();
 
-          Comment.deleteMany({post: req.params.id})
-           .then(function(){
-              return res.redirect('back');
-          }).catch(function(err){
 
-          });
-      }else{
-          return res.redirect('back');
-      }
+module.exports.destroy = async function(req, res){
+  try{
+    let post =  await Post.findById(req.params.id);
+    if (post.user == req.user.id){
+      await post.deleteOne();
 
-  }).catch(function(err){
-    return res.redirect('back')
-  });
+      await Comment.deleteMany({post: req.params.id});
+      return res.redirect('back');
+
+
+    }else{
+        return res.redirect('back');
+    }
+  }catch(err){
+    console.log('Error',err);
+    return;
+  }
 }
+
