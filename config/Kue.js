28845f7484674dcb7e6c522ab042f05b0761cc91
unknown <ravikumargupta391999@gmail.com>
2023-08-23 20:51:21 +0530

    Reddis Server Connected

--- config/Kue.js
+++ config/Kue.js
@@ -1,9 +1,5 @@
 const kue = require('kue');
-// const queue = kue.createQueue();
+const queue = kue.createQueue();
 
-
-var queue = kue.createQueue({
-    redis: 'redis://red-cjj1dnocfp5c73ch4pm0:6379'
-  });
 module.exports = queue;
 
