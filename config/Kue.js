7fa98d39c7c40c3e05f0676be5844acc855a749d
unknown <ravikumargupta391999@gmail.com>
2023-08-23 20:07:51 +0530

    added redis server link

--- config/Kue.js
+++ config/Kue.js
@@ -1,5 +1,9 @@
 const kue = require('kue');
-const queue = kue.createQueue();
+// const queue = kue.createQueue();
 
+
+var queue = kue.createQueue({
+    redis: 'redis://red-cjj1dnocfp5c73ch4pm0:6379'
+  });
 module.exports = queue;
 
